0c21a54438e119e7044bc0d66d9bedfdc64755bc
mohamed amine <mohamedaminebenounn@gmail.com>
2023-09-23 14:36:57 +0200

    creat project

---
+++ main.js
@@ -0,0 +1,8 @@
+var phone = document.querySelector('.phone');
+var container = document.querySelector('.container')
+function iphone (newphone){
+    phone.src=newphone;
+}
+function colors(color){
+    container.style.background = color;
+}
